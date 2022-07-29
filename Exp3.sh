2e55332f9235a04a2432a5d09f63d8d946c1e0ca
Ryana265 <99040464+Ryana265@users.noreply.github.com>
2022-07-29 20:59:19 +0530

    Create Exp3.sh

---
+++ Exp3.sh
@@ -0,0 +1,11 @@
+echo "Enter the first number";
+read a;
+echo "Enter the secound number";
+read b;
+echo "The values are $a $b";
+if [ $a > $b ]
+then
+echo "$a is bigger";
+else
+echo "$b is bigger";
+fi 
